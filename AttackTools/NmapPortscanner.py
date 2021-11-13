55fd5583f053632a5d2c722341442b0aee3b1c42
ben <ben.podawiltz@gmail.com>
2021-11-12 20:03:00 -0800

    added looping menu and exit

--- AttackTools/NmapPortscanner.py
+++ AttackTools/NmapPortscanner.py
@@ -2,7 +2,7 @@
 #
 # script name:  Attack Tools - Nmap port scanning 2 of 3
 # author:       ben_podawiltz
-# revision:     11.7.21
+# revision:     11.12.21
 #
 #######################################
 #
@@ -16,53 +16,67 @@ import nmap
 scanner = nmap.PortScanner()
 #######################################
 
+
+def SynAck ():
+        print("Nmap Version: ", scanner.nmap_version())
+        scanner.scan(ip_addr, range, '-v -sS')
+        print(scanner.scaninfo())
+        print("Ip Status: ", scanner[ip_addr].state())
+        print(scanner[ip_addr].all_protocols())
+        print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+def UDP ():
+        print("Nmap Version: ", scanner.nmap_version())
+        scanner.scan(ip_addr, range, '-v -sU')
+        print(scanner.scaninfo())
+        print("Ip Status: ", scanner[ip_addr].state())
+        print(scanner[ip_addr].all_protocols())
+        print("Open Ports: ", scanner[ip_addr]['udp'].keys())   
+def Comp ():
+        print("Nmap Version: ", scanner.nmap_version())
+        scanner.scan(ip_addr, range, '-v -sS -sV -sC -A -O')
+        # sS for SYN, sv probe open ports, O is for OS type
+        print(scanner.scaninfo())
+        print("Ip Status: ", scanner[ip_addr].state())
+        print(scanner[ip_addr].all_protocols())
+        print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+# Menu options
+
 print("Nmap Automation Tool")
 print("--------------------")
-
 ip_addr = input("IP address to scan: ")
 print("The IP you entered is: ", ip_addr)
 type(ip_addr)
+range = input("Please enter port range for scanning:  ")
 
-resp = input("""\nSelect scan to execute:
+if __name__== "__main__":
+    while True:
+        resp = input("""\nSelect scan to execute:
                 1) SYN ACK Scan
                 2) UDP Scan
-                3) Comprehensive Scam       \n""") 
-print("You have selected option: ", resp)
-
-range = input("Please enter port range for scanning:  ")
-### DONE: Prompt the user to type in a port range for this tool to scan
-
-if resp == '1':
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, range, '-v -sS')
-    print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
-elif resp == '2':
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, range, '-v -sU')
-    print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: ", scanner[ip_addr]['udp'].keys())   
- 
-elif resp == '3':
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, range, '-v -sS -sV -sC -A -O')
-    # sS for SYN, sv probe open ports, O is for OS type
-    print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+                3) Comprehensive Scan        
+                4) Exit                    \n""") 
+        print("You have selected option: ", resp)
     
-elif resp >= '4':
-    print("Error, please make another selection: ")
+
+        if (resp == "1"):
+            SynAck()
+        elif (resp == "2"):
+            UDP ()
+        elif (resp == "3"):
+            Comp ()
+        elif (resp == "4"):
+            print("____Quiting____")
+            break
+        else:
+            print("Error, make another selection:")
 
 
 
-# End
+
+    
+
 
 # Source: https://www.pythonpool/com/python-nmap/
 
 
+# End
